eff06a3bd542a983fb4c14195bb48b8c8c1fdd6b
Sherakama <sherakama@gmail.com>
2017-03-16 15:45:49 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,7 @@ module.exports = function(grunt) {
         includePaths: [
           "node_modules/bourbon/core",
           "node_modules/bourbon-neat/core",
+          "node_modules/neat-omega",
           "node_modules"
         ],
         sourceMap: true,
